80157c4f2593c3378c4706f16cd5a46ce3d46091
saadalish <107108027+saadalish@users.noreply.github.com>
2022-06-23 19:42:59 +0500

    Update views.py

--- views.py
+++ views.py
@@ -1,10 +1,13 @@
-from django.views import generic
+from django.core.paginator import Paginator
+from django.shortcuts import render
 
 from products.models import Product
 
 
-class IndexView(generic.ListView):
-    model = Product
-    paginate_by = 50
-    template_name = 'products/index.html'
-    context_object_name = 'products'
+def index(request):
+    products = Product.objects.all()
+    paginator = Paginator(products, 50)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'products': products, 'page_obj': page_obj}
+    return render(request, 'products/index.html', context)
