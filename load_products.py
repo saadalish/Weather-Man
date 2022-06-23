2e59657ed575a5fd68ae5163128ba2f516c9e1bd
saadalish <107108027+saadalish@users.noreply.github.com>
2022-06-23 19:41:06 +0500

    Update load_products.py

--- load_products.py
+++ load_products.py
@@ -2,7 +2,13 @@ import json
 
 from django.core.management.base import BaseCommand
 
-from products.models import *
+from products.models import (
+    Category,
+    Product,
+    ProductImage,
+    ProductVariant,
+    Vendor
+)
 
 
 class Command(BaseCommand):
@@ -15,7 +21,6 @@ class Command(BaseCommand):
             list_of_products = json.load(json_file)
 
         for single_product_info in list_of_products:
-
             product_category, created = Category.objects.get_or_create(name=single_product_info.get('category'))
             product_vendor, created = Vendor.objects.get_or_create(name=single_product_info.get('vendor'))
 
@@ -23,16 +28,19 @@ class Command(BaseCommand):
                 title=single_product_info.get('title'), category_id=product_category.id, vendor_id=product_vendor.id,
                 details=single_product_info.get('details'), source=single_product_info.get('src')
             )
-
-            for product_variant in single_product_info.get('available-sizes'):
-                ProductVariant.objects.get_or_create(
+            [
+                ProductVariant.objects.create(
                     variant_id=product_variant.get('id'), quantity=product_variant.get('inventory_quantity'),
                     price=product_variant.get('price'), sku=product_variant.get('sku'),
                     size=product_variant.get('title'), product_id=product.id
                 )
-            for product_image in single_product_info.get('images'):
-                ProductImage.objects.get_or_create(
+                for product_variant in single_product_info.get('available-sizes')
+            ]
+            [
+                ProductImage.objects.create(
                     width=product_image.get('width'), product_id=product.id,
                     height=product_image.get('height'), source=product_image.get('src')
                 )
-
+                for product_image in single_product_info.get('images')
+            ]
+        
